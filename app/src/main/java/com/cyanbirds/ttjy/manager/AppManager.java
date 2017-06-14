29e68adb316a25c9434a6cebf6d6c5fb6c050cb2
wangyuebin <wangyb@ecqun.com>
2017-06-14 19:26:35 +0800

    modify bug

--- app/src/main/java/com/cyanbirds/ttjy/manager/AppManager.java
+++ app/src/main/java/com/cyanbirds/ttjy/manager/AppManager.java
@@ -340,6 +340,27 @@ public class AppManager {
 	}
 
 	/**
+	 * 方法描述：判断某一Service是否正在运行
+	 *
+	 * @param context     上下文
+	 * @param serviceName Service的全路径： 包名 + service的类名
+	 * @return true 表示正在运行，false 表示没有运行
+	 */
+	public static boolean isServiceRunning(Context context, String serviceName) {
+		ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+		List<ActivityManager.RunningServiceInfo> runningServiceInfos = am.getRunningServices(50);
+		if (runningServiceInfos.size() <= 0) {
+			return false;
+		}
+		for (ActivityManager.RunningServiceInfo serviceInfo : runningServiceInfos) {
+			if (serviceInfo.service.getClassName().equals(serviceName)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
 	 * 获取当前栈顶的activity的名称
 	 * 
 	 * @param context
