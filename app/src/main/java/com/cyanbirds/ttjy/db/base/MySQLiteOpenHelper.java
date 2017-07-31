eb4512a47f4a2101c5146d7ff26a5d7d685d8ac3
wangyuebin <wangyb@ecqun.com>
2017-07-31 14:01:13 +0800

    modify bug

---
+++ app/src/main/java/com/cyanbirds/ttjy/db/base/MySQLiteOpenHelper.java
@@ -0,0 +1,39 @@
+package com.cyanbirds.ttjy.db.base;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.cyanbirds.ttjy.greendao.ContactDao;
+import com.cyanbirds.ttjy.greendao.ConversationDao;
+import com.cyanbirds.ttjy.greendao.DaoMaster;
+import com.cyanbirds.ttjy.greendao.DynamicDao;
+import com.cyanbirds.ttjy.greendao.ExpressionDao;
+import com.cyanbirds.ttjy.greendao.GoldDao;
+import com.cyanbirds.ttjy.greendao.IMessageDao;
+import com.cyanbirds.ttjy.greendao.NameListDao;
+import com.github.yuweiguocn.library.greendao.MigrationHelper;
+
+import org.greenrobot.greendao.database.Database;
+
+/**
+ * 作者：wangyb
+ * 时间：2017/7/4 22:44
+ * 描述：
+ */
+public class MySQLiteOpenHelper extends DaoMaster.OpenHelper {
+
+	public MySQLiteOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory) {
+		super(context, name, factory);
+	}
+	@Override
+	public void onUpgrade(Database db, int oldVersion, int newVersion) {
+		MigrationHelper.migrate(db,
+				ConversationDao.class,
+				DynamicDao.class,
+				GoldDao.class,
+				IMessageDao.class,
+				NameListDao.class,
+				ContactDao.class,
+				ExpressionDao.class);
+	}
+}
